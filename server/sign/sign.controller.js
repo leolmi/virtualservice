ca281efbc4e2ae4d49ef4da5a021cc93c1eaeb17
olmil <leo.olmi@gmail.com>
2019-04-20 00:12:10 +0200

    varie

--- server/sign/sign.controller.js
+++ server/sign/sign.controller.js
@@ -6,7 +6,7 @@ const mailer = require('nodemailer');
 const User = require('../api/user/user.model');
 const Auth = require('../auth/auth.service');
 const config = require('../config/environment');
-const URL_BASE = 'https://virtualservices.herokuapp.com/?sign=';
+const URL_BASE = 'https://virtualservice.herokuapp.com/?sign=';
 const SUBSCRIPTION_SUBJECT = 'Virtual-Service subscription';
 const SUBSCRIPTION_HTML = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
