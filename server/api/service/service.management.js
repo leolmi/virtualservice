a33a87ad671b299c6ad972750b7119f19d37ce7a
olmil <leo.olmi@gmail.com>
2019-04-22 21:50:48 +0200

    varie

--- server/api/service/service.management.js
+++ server/api/service/service.management.js
@@ -3,11 +3,22 @@ const User = require('../user/user.model');
 const u = require('../../utils');
 const _ = require('lodash');
 
+
+function resetPassword(res, args) {
+  if (!args.password) return u.error(res, 'Undefined pasword!');
+  if ((args.password||'').trim().length<3) return u.error(res, 'Wrong pasword!');
+  const userId = req.user._id;
+  User.findById(userId, (err, user) => {
+    if (err) return u.error(res, err);
+    user.password = args.password;
+    user.save(err => err ? u.error(res, err) : res.send(200));
+  });
+}
+
 const MANAGER = {
-  resetUsers: (res) => User.deleteMany({}, (err) => err ? u.error(res, err) : u.ok(res))
+  resetUsers: (res) => User.deleteMany({}, (err) => err ? u.error(res, err) : u.ok(res)),
+  resetPassword: (res, args) => resetPassword(res)
 };
-
-
 
 exports.execute = function(req, res) {
   if (_.isFunction(MANAGER[req.body.command])) {
